5004066f21eba77b23221907c1b91899da21b6ec
Will Moritz <will@crowdfavorite.com>
2020-09-15 14:01:27 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+var elem = document.getElementById("select-me");
+var text = elem === null || elem === void 0 ? void 0 : elem.textContent;
+// Next up: split up the text into individual nodes.
---
+++ app.ts
@@ -0,0 +1,4 @@
+let elem = document.getElementById("select-me");
+let text = elem?.textContent
+
+// Next up: split up the text into individual nodes.
