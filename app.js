f179f532e59ac1e9c65a43eaedb389eb2e80bc15
lu-cashmoney <lucas.corley@gmail.com>
2020-10-06 14:42:10 -0400

    RANSOMWARE

--- app.js
+++ app.js
@@ -1,19 +1,60 @@
 "use strict";
+function randomColorGen() {
+    const colorArray = ["red", "blue", "yellow", "orange"];
+    const colorSelection = colorArray[Math.floor(Math.random() * colorArray.length)];
+    return colorSelection;
+}
+function setFontColors(elemChar) {
+    let fontColor = randomColorGen();
+    let fontBackgroundColor = randomColorGen();
+    while (fontColor === fontBackgroundColor) {
+        fontColor = randomColorGen();
+        fontBackgroundColor = randomColorGen();
+    }
+    elemChar.style.setProperty("color", fontColor);
+    elemChar.style.setProperty("background-color", fontBackgroundColor);
+    if (elemChar.textContent === " ") {
+        elemChar.style.setProperty("background-color", "");
+    }
+}
+function randomPixelSizeGen(multiplier, sizeFloor) {
+    const randomNum = Math.floor(Math.random() * multiplier);
+    return randomNum > sizeFloor
+        ? randomNum.toString() + "px"
+        : sizeFloor.toString() + "px";
+}
+function randomFontCapitalizationGen() {
+    return Math.round(Math.random()) === 1 ? "uppercase" : "lowercase";
+}
+function randomFontGen() {
+    const fontFamilys = [];
+}
 function decompose(elem) {
-    var text = elem.textContent;
-    var textArray = text.split("");
-    var textArrayElems = textArray === null || textArray === void 0 ? void 0 : textArray.map(function (char) {
-        var elemChar = document.createElement("span");
+    let text = elem.textContent;
+    let textArray = text?.split("");
+    let textArrayElems = textArray?.map((char) => {
+        const elemChar = document.createElement("span");
         elemChar.textContent = char;
-        elemChar.className = "test";
-        elem.parentNode.insertBefore(elemChar, elem);
+        //elemChar.className = "test";
+        setFontColors(elemChar);
+        elemChar.style.setProperty("text-transform", randomFontCapitalizationGen());
+        elemChar.style.setProperty("top", randomPixelSizeGen(3, 1));
+        elemChar.style.setProperty("left", randomPixelSizeGen(3, 1));
+        elemChar.style.setProperty("right", randomPixelSizeGen(3, 1));
+        elemChar.style.setProperty("bottom", randomPixelSizeGen(3, 1));
+        elemChar.style.setProperty("font-size", randomPixelSizeGen(40, 20));
+        // elemChar.className = Math.round(Math.random()) === 1 ? "test" : "test_alt";
+        elem.parentNode?.insertBefore(elemChar, elem);
         return elemChar;
     });
-    elem.parentNode.removeChild(elem);
+    elem.parentNode?.removeChild(elem);
 }
 function textFilter(node) {
     if (["p", "span", "label", "a", "h*"].includes(node.parentElement.localName)) {
         return NodeFilter.FILTER_ACCEPT;
+    }
+    else {
+        return NodeFilter.FILTER_SKIP;
     }
 }
 function getTextNodes() {
@@ -23,8 +64,8 @@ function getTextNodes() {
         a.push(n);
     return a;
 }
-var textNodes = getTextNodes();
-for (var index = 0; index < textNodes.length; index++) {
-    var textNode = textNodes[index];
+let textNodes = getTextNodes();
+for (let index = 0; index < textNodes.length; index++) {
+    const textNode = textNodes[index];
     decompose(textNode);
 }
--- app.ts
+++ app.ts
@@ -1,26 +1,83 @@
-function decompose(elem) {
-  let text = elem.textContent;
-  let textArray = text.split("");
+function randomColorGen() {
+  const colorArray = ["red", "blue", "yellow", "orange"];
+  const colorSelection =
+    colorArray[Math.floor(Math.random() * colorArray.length)];
+  return colorSelection;
+}
 
-  let textArrayElems = textArray?.map((char) => {
+function setFontColors(elemChar: HTMLSpanElement) {
+  let fontColor = randomColorGen();
+  let fontBackgroundColor = randomColorGen();
+
+  while (fontColor === fontBackgroundColor) {
+    fontColor = randomColorGen();
+    fontBackgroundColor = randomColorGen();
+  }
+
+  elemChar.style.setProperty("color", fontColor);
+  elemChar.style.setProperty("background-color", fontBackgroundColor);
+
+  if (elemChar.textContent === " ") {
+    elemChar.style.setProperty("background-color", "");
+  }
+}
+
+function randomPixelSizeGen(multiplier: number, sizeFloor: number) {
+  const randomNum = Math.floor(Math.random() * multiplier);
+
+  return randomNum > sizeFloor
+    ? randomNum.toString() + "px"
+    : sizeFloor.toString() + "px";
+}
+
+function randomFontCapitalizationGen() {
+  return Math.round(Math.random()) === 1 ? "uppercase" : "lowercase";
+}
+
+function randomFontGen() {
+  const fontFamilys = [];
+}
+
+function decompose(elem: Node) {
+  let text = elem.textContent;
+  let textArray = text?.split("");
+
+  let textArrayElems = textArray?.map((char: string) => {
     const elemChar = document.createElement("span");
     elemChar.textContent = char;
-    elemChar.className = "test";
-    elem.parentNode.insertBefore(elemChar, elem);
+    //elemChar.className = "test";
+    setFontColors(elemChar);
+    elemChar.style.setProperty("text-transform", randomFontCapitalizationGen());
+    elemChar.style.setProperty("top", randomPixelSizeGen(3, 1));
+    elemChar.style.setProperty("left", randomPixelSizeGen(3, 1));
+    elemChar.style.setProperty("right", randomPixelSizeGen(3, 1));
+    elemChar.style.setProperty("bottom", randomPixelSizeGen(3, 1));
+    elemChar.style.setProperty("font-size", randomPixelSizeGen(40, 20));
+    // elemChar.className = Math.round(Math.random()) === 1 ? "test" : "test_alt";
+    elem.parentNode?.insertBefore(elemChar, elem);
     return elemChar;
   });
 
-  elem.parentNode.removeChild(elem);
+  elem.parentNode?.removeChild(elem);
 }
 
-function textFilter(node) {
-  if (["p", "span", "label", "a", "h*"].includes(node.parentElement.localName)) {
+function textFilter(node: Node) {
+  if (
+    ["p", "span", "label", "a", "h*"].includes(node.parentElement!.localName)
+  ) {
     return NodeFilter.FILTER_ACCEPT;
+  } else {
+    return NodeFilter.FILTER_SKIP;
   }
 }
 
 function getTextNodes() {
-  var walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, { acceptNode: textFilter }, true);
+  var walk = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_TEXT,
+    { acceptNode: textFilter },
+    true
+  );
   var n,
     a = [];
 
