0177a5135402cfc68b2524093818076f51efc43c
lu-cashmoney <lucas.corley@gmail.com>
2020-10-23 14:18:49 -0400

    another merge fix

--- app.ts
+++ app.ts
@@ -317,5 +317,5 @@ async function slowlyLoseItAll(className: string) {
     }
 }
 
-ransomifyAll('all-chars');
-slowlyLoseItAll('all-chars');
+// ransomifyAll('all-chars');
+// slowlyLoseItAll('all-chars');
